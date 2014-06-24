c4ae627dcdcb1095056317c7bd25f19eabbc7804
Anson Yu Wang <opensource@undozen.com>
2014-06-24 21:58:43 +0800

    update bootstrap.sql

--- models/bootstrap.sql
+++ models/bootstrap.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE mbzprod DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; 
-GRANT ALL ON mbzprod.* TO mbzprod@localhost IDENTIFIED BY 'mbzPROD';
+CREATE DATABASE mbztest DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; 
+GRANT ALL ON mbztest.* TO mbztest@localhost IDENTIFIED BY 'mbzTEST';
