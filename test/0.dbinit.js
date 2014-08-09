911e507c12f3754ad2e68264f13902d9f6d73330
Anson Yu Wang <opensource@undozen.com>
2014-08-09 21:35:58 +0800

    use simple-migrate module

--- test/0.dbinit.js
+++ test/0.dbinit.js
@@ -2,30 +2,20 @@ var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
-var dbconnection = require('mysql').createConnection(_.merge(
-      require('config').mysqlConnection,
-      {multipleStatements: true}
-    ));
+var sm = require('simple-migrate');
 var qdb = require('../lib/db/qdb');
 describe('start', function () {
   before(function (done) {
-    var sql = fs.readdirSync(__dirname + '/../db/migration')
-    .filter(function (filename) {
-      if (!filename.match(/\.sql$/i)) return false;
-      if (isNaN(Date.parse(filename.substring(0, 24)))) return false;
-      return true;
-    })
-    .map(function (filename) {
-      return fs.readFileSync(path.resolve(__dirname, '..', 'db', 'migration', filename), 'utf-8');
-    })
-    .reduce(function (r, sql) {
-      return r + sql;
-    }, '');
     qdb.flushall()
     .then(function () {
-      dbconnection.connect();
-      dbconnection.query(sql);
-      dbconnection.end(done);
+      sm(
+        _.merge(
+          require('config').mysqlConnection,
+          {multipleStatements: true}
+        ),
+        __dirname + '/../db/migration',
+        new Date(0),
+        done);
     }, done)
     .done();
   });
